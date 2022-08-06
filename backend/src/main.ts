a5ba217a59b769d7178e92ba91abdc9e0d44ff0f
musthaq96 <mushtaq96smb@gmail.com>
2022-08-06 14:05:04 +0200

    add fresh backend starter code

---
+++ backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('Week 4 Homework Group 6')
+    .setVersion('1.0.0')
+    .setDescription('IPFS NFT project for week 4 of Encode Solidity bootcamp')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('docs', app, document);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+}
+bootstrap();
